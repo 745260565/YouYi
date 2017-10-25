8a29737bbcd9f583977b3a4444aab90a5045619a
265G-mac <chenghao@chenghaodeMac-mini.local>
2017-10-25 16:42:43 +0800

    创建项目

---
+++ YouYi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouYi
+//
+//  Created by chenghao on 2017/10/25.
+//  Copyright © 2017年 com.72g.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
